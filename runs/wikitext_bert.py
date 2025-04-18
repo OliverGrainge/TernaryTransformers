df28ed2542e0e70fa8e4034f093aadad6d1f2d5a
olivergraininge <oliver@grainge.me>
2025-04-18 14:08:51 +0100

    adding runs

---
+++ runs/wikitext_bert.py
@@ -0,0 +1,42 @@
+import multiprocessing as mp
+import os
+import sys
+
+# Add parent directory to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dataloaders.MaskedLanguageModelling import Wikitext2DataModule
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.cli import LightningCLI
+from trainers import BertModule
+
+mp.set_start_method("forkserver", force=True)
+
+
+def main():
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        mode="min",
+        save_top_k=1,
+        filename="{epoch}-{val_loss:.2f}",
+        every_n_epochs=1,
+        dirpath="./checkpoints/cifar10_classification/",
+    )
+
+    LightningCLI(
+        model_class=BertModule,
+        datamodule_class=Wikitext2DataModule,
+        trainer_defaults={
+            "max_epochs": 20,
+            "accelerator": "auto",
+            "precision": "16-mixed",
+            "devices": 1,
+            "log_every_n_steps": 10,
+            "callbacks": [checkpoint_callback],
+        },
+        save_config_callback=None,
+    )
+
+
+if __name__ == "__main__":
+    main()
